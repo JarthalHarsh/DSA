e27fd7f8caf1da32e1b99dbc84c01496c2cb67b7
JarthalHarsh <jarthalharsh7@gmail.com>
2025-01-09 22:17:06 +0530

    Partition and quicksort

--- Sorting.cpp
+++ Sorting.cpp
@@ -81,11 +81,62 @@ void InsertionSort(vector<int> arr)
       cout<<val<<",";
    }
 }
+
+int partitionArr(vector<int> &arr, int start, int end)
+{
+   //{5,3,1,4,2,18,3,6,8,11,12}
+   int n = end+1;
+   int partEle = arr[n-1];
+   int j = start-1;
+   for(int i = start; i < n; i++)
+   {
+       if(arr[i] <= partEle)
+       {
+         int k = i;
+         int temp = arr[i];
+         while(k > j+1)
+         {
+            arr[k] = arr[k-1];
+            k--;
+         }
+         arr[k] = temp;
+         j++;
+         if(i == n-1)
+         {
+           return k;  
+         }
+       }
+       
+   }
+   return -1;
+} 
+
+void quickSort(vector<int> &arr, int start, int end)
+{
+   if(start > end)
+   {
+      return;
+   }
+   int index = partitionArr(arr,start,end);
+   quickSort(arr,start,index-1);
+   quickSort(arr,index+1,end);
+}
+
+
 int main() {
-    vector<int> arr = {5,3,1,4,2,18,3,6,8,11,12};
+    vector<int> arr = {5,3,1,4,2,18,3,6,8,11,7};
     //bubblesort(arr);
     //selectionSort(arr);
 
-    InsertionSort(arr);
+    //InsertionSort(arr);
+
+    //cout<<partitionArr(arr,0,10);
+    quickSort(arr,0,10);
+
+    for(int val : arr)
+   {
+      cout<<val<<",";
+   }
+
     return 0;
 }
